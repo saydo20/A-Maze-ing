15c9f1910144e3f9b0f335716f1eff3011ba5a64
saydo20 <jdiasaad86@gmail.com>
2026-01-28 13:07:10 +0100

    the main program that call all the modules and exicute them, and it print the output in file maze.txt

---
+++ a_maze_ing.py
@@ -0,0 +1,15 @@
+import sys
+import parsing
+import maze_generation
+
+arg = sys.argv
+if len(arg) != 2:
+    raise ValueError("the prgram must has two argument")
+else:
+    dict = parsing.convert_dict(arg[1])
+    arr = maze_generation.MazeGenerator.create_grid(dict)
+    with open(dict["OUTPUT_FILE"], "w") as f:
+        for row in arr:
+            for cell in row:
+                f.write(str(cell))
+            f.write("\n")

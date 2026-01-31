f9470b64ddd215c23cc092078ac29bc21558b56b
saydo20 <jdiasaad86@gmail.com>
2026-01-31 07:47:07 +0100

    the program now return the maze ass 2D array and the path as string

--- a_maze_ing.py
+++ a_maze_ing.py
@@ -7,9 +7,15 @@ if len(arg) != 2:
     raise ValueError("the prgram must has two argument")
 else:
     dict = parsing.convert_dict(arg[1])
-    arr = maze_generation.MazeGenerator.create_grid(dict)
+    arr, path = maze_generation.MazeGenerator.create_grid(dict)
     with open(dict["OUTPUT_FILE"], "w") as f:
         for row in arr:
             for cell in row:
                 f.write(str(cell))
             f.write("\n")
+        f.write("\n")
+        f.write(str(tuple(dict["ENTRY"])))
+        f.write("\n")
+        f.write(str(tuple(dict["EXIT"])))
+        f.write("\n")
+        f.write(path)

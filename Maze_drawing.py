317fb9d34a9e3cab4edbf9c63cddee16088c7d6c
ymouafak <ymouafak@e2r10p4.1337.ma>
2026-02-01 22:10:51 +0100

    drawing first steps

--- Maze_drawing.py
+++ Maze_drawing.py
@@ -8,6 +8,15 @@ class Draw:
             self.file = file
             self.screen = screen
 
+    def print_grid(self):
+        i = 0
+        while(i < self.heigth):
+            j = 0
+            while(j < self.width):
+                self.print_walls(int('F', 16), i, j)
+                j += 1
+            i += 1
+
     def iterate(self):
         with open(self.file, 'r') as file:
             height = 0
@@ -17,6 +26,8 @@ class Draw:
                 for char in str:
                     if(width < self.width):
                         self.print_walls(int(char, 16), height, width)
+                        self.screen.refresh()
+                        curses.napms(100)
                     width += 1
                 height += 1
 
@@ -24,27 +35,41 @@ class Draw:
         
         x = height * 3
         y = width * 4
+
         if cell_wals & (1 << 0):
-            self.screen.addstr(x, y,"+━━━")
+            self.screen.addstr(x, y," ━━━")
+        else:
+            self.screen.addstr(x, y,"    ")
         if cell_wals & (1 << 3):
             self.screen.addstr(x + 1, y,"┃")
             self.screen.addstr(x + 2, y,"┃")
+        else :
+            self.screen.addstr(x + 1, y," ")
+            self.screen.addstr(x + 2, y," ")
         if height == self.heigth - 1:
             if cell_wals & (1 << 2):
-                self.screen.addstr(x + 3, y,"+━━━")
+                self.screen.addstr(x + 3, y," ━━━")
+            else:
+                self.screen.addstr(x, y,"    ")
         if width == self.width - 1:
             if cell_wals & (1 << 1):
                 self.screen.addstr(x + 1, y + 4,"┃")
                 self.screen.addstr(x + 2, y + 4,"┃")
+            else :
+                self.screen.addstr(x + 1, y," ")
+                self.screen.addstr(x + 2, y," ")
 
 
 
 
 def main(stdscr):
     stdscr.clear()
-    # stdscr.curses(0)
+    curses.curs_set(0)
 
     draw = Draw(5, 5, "maze.txt", stdscr)
+    draw.print_grid()
+    stdscr.refresh()
+    curses.napms(100)
     draw.iterate()
     stdscr.refresh()
     stdscr.getkey()

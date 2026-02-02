b9d68a8ceaef27fa6417fb5e4661cd1a9bb3b163
saydo20 <jdiasaad86@gmail.com>
2026-02-02 18:32:06 +0100

    nothing new

--- a_maze_ing.py
+++ a_maze_ing.py
@@ -1,21 +1,23 @@
 import sys
 import parsing
 import maze_generation
-
-arg = sys.argv
-if len(arg) != 2:
-    raise ValueError("the prgram must has two argument")
-else:
-    dict = parsing.convert_dict(arg[1])
-    arr, path = maze_generation.MazeGenerator.create_grid(dict)
-    with open(dict["OUTPUT_FILE"], "w") as f:
-        for row in arr:
-            for cell in row:
-                f.write(str(cell))
+try:
+    arg = sys.argv
+    if len(arg) != 2:
+        raise ValueError("the prgram must has two argument")
+    else:
+        dict = parsing.convert_dict(arg[1])
+        arr, path = maze_generation.MazeGenerator.create_grid(dict)
+        with open(dict["OUTPUT_FILE"], "w") as f:
+            for row in arr:
+                for cell in row:
+                    f.write(str(cell.value))
+                f.write("\n")
             f.write("\n")
-        f.write("\n")
-        f.write(str(tuple(dict["ENTRY"])))
-        f.write("\n")
-        f.write(str(tuple(dict["EXIT"])))
-        f.write("\n")
-        f.write(path)
+            f.write(str(tuple(dict["ENTRY"])))
+            f.write("\n")
+            f.write(str(tuple(dict["EXIT"])))
+            f.write("\n")
+            f.write(path)
+except Exception as Error:
+    print(f"caught an error : {Error}")

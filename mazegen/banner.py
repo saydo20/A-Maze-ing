9ff89e9b69c2b758edafa88a3ebce5a295050448
ymouafak <ymouafak@e2r8p2.1337.ma>
2026-02-14 00:52:40 +0100

    flake8 and mypy done

--- mazegen/banner.py
+++ mazegen/banner.py
@@ -19,7 +19,6 @@ def main(stdscr: Any) -> None:
     curses.init_pair(1, curses.COLOR_YELLOW, curses.COLOR_BLACK)
     curses.init_pair(2, curses.COLOR_CYAN, curses.COLOR_BLACK)
     curses.init_pair(3, curses.COLOR_BLACK, curses.COLOR_WHITE)
-    curses.init_pair(4, curses.COLOR_WHITE, curses.COLOR_BLACK)
 
     art = [
         "  ██          ██   ██   ██   ██████ ██████"
@@ -44,57 +43,56 @@ def main(stdscr: Any) -> None:
     menu_items = ["Start", "Quit"]
     selected = 0
 
+    stdscr.clear()
+    h, w = stdscr.getmaxyx()
+
+    art_start_y = h // 6
+    i = 0
+    for line in art:
+        x = w // 2 - len(line) // 2
+        y = art_start_y + i
+        stdscr.addstr(y, x, line, curses.color_pair(1))
+        i += 1
+
+    info_start_y = art_start_y + 6 + 2
+    i = 0
+    for line in info_lines:
+        x = w // 2 - len(line) // 2
+        y = info_start_y + i
+        stdscr.addstr(y, x, line, curses.color_pair(2))
+        i += 1
+
+    help_text = "↑↓ Move | ENTER Select | Q Quit"
+    x_help = w // 2 - len(help_text) // 2
+    stdscr.addstr(h - 2, x_help, help_text)
+
     while True:
-        stdscr.clear()
-        h, w = stdscr.getmaxyx()
 
-        art_start_y = h // 6
-        for i, line in enumerate(art):
-            x = w // 2 - len(line) // 2
-            y = art_start_y + i
-            stdscr.attron(curses.color_pair(1))
-            stdscr.addstr(y, x, line)
-            stdscr.attroff(curses.color_pair(1))
-
-        info_start_y = art_start_y + len(art) + 2
-        for i, line in enumerate(info_lines):
-            x = w // 2 - len(line) // 2
-            y = info_start_y + i
-            stdscr.attron(curses.color_pair(2))
-            stdscr.addstr(y, x, line)
-            stdscr.attroff(curses.color_pair(2))
-
-        menu_start_y = info_start_y + len(info_lines) + 2
-        for i, item in enumerate(menu_items):
+        menu_start_y = info_start_y + 4
+        i = 0
+        for item in menu_items:
             x = w // 2 - len(item) // 2
             y = menu_start_y + i
             if i == selected:
-                stdscr.attron(curses.color_pair(3))
-                stdscr.addstr(y, x, item)
-                stdscr.attroff(curses.color_pair(3))
+                stdscr.addstr(y, x, item, curses.color_pair(3))
             else:
-                stdscr.attron(curses.color_pair(4))
                 stdscr.addstr(y, x, item)
-                stdscr.attroff(curses.color_pair(4))
-
-        help_text = "↑↓ Move | ENTER Select | Q Quit"
-        x_help = w // 2 - len(help_text) // 2
-        stdscr.addstr(h - 2, x_help, help_text)
+            i += 1
 
         stdscr.refresh()
-        key = stdscr.getch()
+        key = stdscr.getkey()
 
-        if key == curses.KEY_UP or key == curses.KEY_DOWN:
-            selected = (selected + 1) % len(menu_items)
+        if key == "KEY_UP" or key == "KEY_DOWN":
+            selected = 1 - selected
 
-        elif key == 10:
+        elif key == "\n":
             if menu_items[selected] == "Quit":
                 exit(0)
             else:
                 return
 
-        elif key in (ord('q'), ord('Q')):
-            break
+        elif key in ('q', 'Q'):
+            exit(0)
 
 
 def run() -> None:
